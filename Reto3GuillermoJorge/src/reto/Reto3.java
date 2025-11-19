f873ac59030f47ebc2ed5d544ef1858091e8e8d9
guillemdb1994-pixel <Guille@Laptop-GMB>
2025-11-19 13:59:41 +0100

    aqweqw

--- Reto3GuillermoJorge/src/reto/Reto3.java
+++ Reto3GuillermoJorge/src/reto/Reto3.java
@@ -1,8 +1,33 @@
 package reto;
 
+import java.util.Scanner;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		int key=99;
+		while (key!=0) {
+			System.out.println("1. Analizar Palabra");
+			System.out.println("2. Analizar Frase");
+			System.out.println("0. Salir");
+			key=Integer.parseInt(sc.nextLine());
+			switch (key) {
+			case 1:
+				Funciones1.pideNombres(sc);
+				break;
+			case 2:
+				
+				break;
+			case 0:
+				key=0;
+				break;
+			default:
+				System.out.println("Opción no valida");
+				break;
+			}
+		}
 	}
 
 }
